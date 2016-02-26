2fec46d37452bea638acfa71b7c077caa734b079
Timofey Dergachev <t@exeto.me>
2016-02-26 18:17:54 +0300

    Add Root container

--- source/containers/App/index.js
+++ source/containers/App/index.js
@@ -1,5 +1,44 @@
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./App.prod');
-} else {
-  module.exports = require('./App.dev');
+import React, { Component, PropTypes } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+
+import * as TodoActions from '../../actions';
+import Header from '../../components/Header';
+import MainSection from '../../components/MainSection';
+import style from './style.css';
+
+class App extends Component {
+  render() {
+    const { todos, actions, children } = this.props;
+    return (
+      <div className={style.normal}>
+        <Header addTodo={actions.addTodo} />
+        <MainSection todos={todos} actions={actions} />
+        {children}
+      </div>
+    );
+  }
 }
+
+App.propTypes = {
+  todos: PropTypes.array.isRequired,
+  actions: PropTypes.object.isRequired,
+  children: PropTypes.object,
+};
+
+function mapStateToProps(state) {
+  return {
+    todos: state.todos,
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(TodoActions, dispatch),
+  };
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
--- source/index.js
+++ source/index.js
@@ -1,20 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Router, Route, browserHistory } from 'react-router';
+import { browserHistory } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
-import { Provider } from 'react-redux';
 
-import App from './containers/App';
 import configure from './store';
+import Root from './containers/Root';
 
 const store = configure();
 const history = syncHistoryWithStore(browserHistory, store);
 
 render(
-  <Provider store={store}>
-    <Router history={history}>
-      <Route path="/" component={App} />
-    </Router>
-  </Provider>,
-  document.getElementById('app')
+  <Root store={store} history={history} />,
+  document.getElementById('root')
 );
