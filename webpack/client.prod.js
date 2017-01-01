0b314b94430f84d66724a86bdff45c10269c1131
Timofey Dergachev <t@exeto.me>
2017-01-01 20:05:37 +0300

    Change settings to uglifyjs plugin

--- webpack/client.prod.js
+++ webpack/client.prod.js
@@ -43,9 +43,16 @@ module.exports = merge({
 
     new webpack.optimize.UglifyJsPlugin({
       compress: {
+        screw_ie8: true,
         warnings: false,
       },
-      comments: false,
+      mangle: {
+        screw_ie8: true,
+      },
+      output: {
+        comments: false,
+        screw_ie8: true,
+      },
     }),
 
     new webpack.DefinePlugin({
