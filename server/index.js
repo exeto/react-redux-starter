5e39750eff5d51fac88b8a82eaf29d79a0c81a6b
Timofey Dergachev <t@exeto.me>
2017-04-02 11:45:40 +0300

    Replace express to koa for server rendering

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 import 'isomorphic-fetch';
 
 import path from 'path';
-import Express from 'express';
+import Koa from 'koa';
+import views from 'koa-ejs';
+import serve from 'koa-static';
 import React from 'react';
 import { renderToString } from 'react-dom/server';
 import { StaticRouter } from 'react-router';
@@ -13,43 +15,51 @@ import actionsTopstories from '../client/actions/topstories';
 import assets from '../tmp/assets.json';
 import config from '../webpack/config';
 
-const app = new Express();
+const app = new Koa();
 
-app.set('views', './server');
-app.set('view engine', 'ejs');
-app.set('x-powered-by', false);
-
-app.use('/', Express.static(path.resolve(__dirname, '../public')));
-
-app.get('*', (req, res) => {
-  const store = configureStore();
-
-  store.dispatch(actionsTopstories.fetchAll())
-    .then(() => {
-      const context = {};
-
-      const html = renderToString(
-        <Provider store={store}>
-          <StaticRouter
-            location={req.url}
-            context={context}
-          >
-            <Routes />
-          </StaticRouter>
-        </Provider>
-      );
-
-      const initialState = store.getState();
-
-      if (context.url) {
-        res.redirect(context.url);
-      } else {
-        res.status(context.status || 200);
-        res.render('template', { assets, data: { html, initialState } });
-      }
-    });
+views(app, {
+  root: './server',
+  layout: false,
+  viewExt: 'ejs',
+  cache: false,
 });
 
-app.listen(config.port, () => {
+app.use(serve(path.resolve(__dirname, '../public')));
+
+app.use(async (ctx) => {
+  if (ctx.method !== 'GET') {
+    ctx.status = 405;
+    return;
+  }
+
+  const store = configureStore();
+
+  await store.dispatch(actionsTopstories.fetchAll());
+
+  const routerContext = {};
+
+  const html = renderToString(
+    <Provider store={store}>
+      <StaticRouter
+        location={ctx.url}
+        context={routerContext}
+      >
+        <Routes />
+      </StaticRouter>
+    </Provider>
+  );
+
+  const initialState = store.getState();
+
+  if (routerContext.url) {
+    ctx.redirect(routerContext.url);
+  } else {
+    ctx.status = routerContext.status || 200;
+    await ctx.render('template', { assets, data: { html, initialState } });
+  }
+});
+
+app.listen(config.port, (err) => {
+  if (err) { return console.log(err); }
   console.log(`==> 🌎  Server running at ${config.publicPath}`);
 });
